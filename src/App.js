dcb124f071503c27d83e9e57ad1bafac6495064f
Henelllis@hellis1001@gmail.com <hellis1001@gmail.com>
2018-08-20 14:44:10 -0400

    Basic bootstrap formating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+
+import Contact from './components/Contact';
+import Header from './components/Header';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
+    
     return (
       <div className="App">
-          <h1 className="App-title">Welcome to React</h1>
+          <Header branding="Contact Manager"/>
+          <div className="container">
+            <Contact name="John Doe"
+                    email="jdoe@gmail.com"
+                    phone="555-5555-5555"/>
+            <Contact name="Karen Smith"
+                    email="ksmith@gmail.com"
+                    phone="555-4444-5555"/>
+          </div>
       </div>
     );
   }
