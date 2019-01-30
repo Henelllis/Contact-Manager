393ea8112e28ab491c0df3a53fd5921676b5ec1f
Henelllis@hellis1001@gmail.com <hellis1001@gmail.com>
2019-01-29 22:26:27 -0500

    Ability to show and hide contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,17 +2,34 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
-    render() {
 
+    state ={
+        showContact: true
+    }
+
+    onClickShowContact = () => {
+        this.setState({showContact: !this.state.showContact})
+    }
+
+    render() {
+        const { showContact } = this.state
         const {name, email, phone } = this.props.contact;
 
-        return (
-            <div className="card card-body mb-3">
-                <h4>{name}</h4>
+        let contactRender = null
+
+        if (showContact){
+            contactRender = (
                 <ul className=" list-group">
                     <li className="list-group-item">{email}</li>
                     <li className="list-group-item">{phone}</li>
                 </ul>
+            )
+        }
+
+        return (
+            <div className="card card-body mb-3">
+                <h4>{name} <i onClick={this.onClickShowContact} className="fas fa-sort-down"></i></h4>
+                {contactRender}
             </div>
         );
     }
